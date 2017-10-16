e36ab79382920ceaf870d28130aee38396ca17b7
Kristen Peck <kpeck71@gmail.com>
2017-10-16 20:18:15 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,2 +1,19 @@
 module Dance
+
+  def twirling
+    "I'm twirling!"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def piroutte
+    "I'm doing a piroutte"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleaure to dance for you all"
+  end
+  
 end

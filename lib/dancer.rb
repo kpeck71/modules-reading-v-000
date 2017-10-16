fbb29a40b54b0e8a5f0f26bd12b6ef8356d59fff
Kristen Peck <kpeck71@gmail.com>
2017-10-16 20:19:30 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance
+  include Dance #use all of the methods of the included module as instance methods. 
 
   attr_accessor :name
 
